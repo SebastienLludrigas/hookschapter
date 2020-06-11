09934c510916acbd2b598cacf15f8ebcd7f09816
Sebastien Lludrigas <sebastienlludrigas@gmail.com>
2020-06-11 08:53:20 +0200

    14/79 terminé

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Composants/Navbar';
-import Compteur from './Composants/Compteur';
+import Counter from './Composants/Counter';
 
 
 function App() {
 
   return (
     <div className="App"> 
-      <Navbar />
-      <Compteur />
+      <Counter/>
     </div>
   );
 }
